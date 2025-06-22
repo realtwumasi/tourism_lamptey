47c3d88da094bb8278a21744e94823d220bb795b
Michael Twumasi <therealtwumasimichael@gmail.com>
2025-06-22 12:57:18 +0100

    new update

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.querySelector(".contact-form").addEventListener("submit", function(e) {
   alert("✅ Your message was sent successfully!");
   this.reset();
 });
+document.querySelectorAll('.accordion-header').forEach(btn => {
+      btn.addEventListener('click', function() {
+        const content = this.nextElementSibling;
+        this.classList.toggle('active');
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          document.querySelectorAll('.accordion-content').forEach(c => c.style.maxHeight = null);
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    });
